f10f8468494f8f810cb8e3a201733449ec56e571
Alex Cambuy <alexcambuy@gmail.com>
2018-05-14 08:35:46 -0300

    Ajustes páginas e rotas e criação da pagina de cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+const appRoutes: Routes = [
+  //home
+  {
+    path: '',
+    component: HomeComponent
+},
+  {
+    path: 'home',
+    component: HomeComponent
+},
+//Cadastro
+{
+    path: 'cadastro',
+    component: CadastroComponent
+}   
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
